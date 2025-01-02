ecaec061d811ae3e4e242acbe66a7d513b16417e
krewi1 <kratochvilp@post.cz>
2025-01-02 21:25:24 +0100

    install on windows

--- init_client.py
+++ init_client.py
@@ -1,19 +1,23 @@
+import dotenv as d
+
 import os
 import MetaTrader5 as mt5
+
+config = d.Dotenv(".env")
 
 def initialize_client():
 
     if not mt5.initialize(
         # replace with path to terminal.exe
-        path=os.getenv("PATH_TO_TERMINAL_EXECUTABLE")
+        path=config.get("PATH_TO_TERMINAL_EXECUTABLE")
     ):
-        print("initialize() failed")
+        print("initialize() failed", mt5.last_error())
         quit()
 
     if not mt5.login(
-        login=os.getenv("LOGIN"),
-        password=os.getenv("PASSWORD"),
-        server=os.getenv("SERVER")
+        login=config.get("LOGIN"),
+        password=config.get("PASSWORD"),
+        server=config.get("SERVER")
     ):
         print("failed to authorize")
         quit()
